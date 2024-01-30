57bf1974d2b7cee3655c302ca902a19f0c613703
simin <inho1019@naver.com>
2024-01-30 21:17:18 +0900

    이메일 인증 하는 중

---
+++ src/main/java/ham/controller/EmailController.java
@@ -0,0 +1,25 @@
+package ham.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ham.service.EmailService;
+import jakarta.mail.MessagingException;
+
+@RestController
+public class EmailController {
+
+	@Autowired
+    private EmailService emailService;
+
+    @PostMapping("user/email")
+    public String mailConfirm(@RequestBody String email) throws UnsupportedEncodingException, MessagingException {
+
+        String authCode = emailService.sendEmail(email);
+        return authCode;
+    }
+}
