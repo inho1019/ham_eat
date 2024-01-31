a9f2f3c491399b83e4ea6398428b08daa05afb93
simin <inho1019@naver.com>
2024-01-31 13:30:28 +0900

    템플릿 변경 및 인증코드 변경

--- src/main/java/ham/service/EmailService.java
+++ src/main/java/ham/service/EmailService.java
@@ -25,26 +25,15 @@ public class EmailService {
 
     private String authNum; //랜덤 인증 코드
 
-    //랜덤 인증 코드 생성
+    // 랜덤 인증 코드 생성
     public void createCode() {
         Random random = new Random();
-        StringBuffer key = new StringBuffer();
+        StringBuilder key = new StringBuilder();
 
-        for(int i=0;i<8;i++) {
-            int index = random.nextInt(3);
-
-            switch (index) {
-                case 0 :
-                    key.append((char) ((int)random.nextInt(26) + 97));
-                    break;
-                case 1:
-                    key.append((char) ((int)random.nextInt(26) + 65));
-                    break;
-                case 2:
-                    key.append(random.nextInt(9));
-                    break;
-            }
+        for (int i = 0; i < 6; i++) {
+            key.append(random.nextInt(10)); // 0부터 9까지의 랜덤 숫자 추가
         }
+
         authNum = key.toString();
     }
 
