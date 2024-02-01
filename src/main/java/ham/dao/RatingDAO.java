1e0d34afae753d8ba86a9a568ec6017325f78743
simin <inho1019@naver.com>
2024-02-02 00:58:30 +0900

    join 수정

--- src/main/java/ham/dao/RatingDAO.java
+++ src/main/java/ham/dao/RatingDAO.java
@@ -12,9 +12,12 @@ import ham.bean.RatingDTO;
 @Repository
 public interface RatingDAO extends JpaRepository<RatingDTO,Integer> {
 
+	@Query("SELECT new ham.bean.RatingDTO(r.ratingSeq, r.burgerSeq, r.userSeq, r.type, r.rate, r.content, r.logTime, u.name, u.birth, u.gender) "
+			+ "FROM RatingDTO r JOIN UserDTO u ON r.userSeq = u.userSeq WHERE r.type = :type ORDER BY r.ratingSeq DESC")
 	List<RatingDTO> findAllByTypeOrderByRatingSeqDesc(@Param("type")int type);
 
-	@Query("SELECT r.*, u.name as user_name FROM rating r JOIN user u ON r.user_seq = u.user_seq WHERE r.burger_seq = :burger_seq")
-	List<RatingDTO> findAllByBurgerSeqOrderByRatingSeqDesc(long burgerSeq);
+	@Query("SELECT new ham.bean.RatingDTO(r.ratingSeq, r.burgerSeq, r.userSeq, r.type, r.rate, r.content, r.logTime, u.name, u.birth, u.gender) "
+			+ "FROM RatingDTO r JOIN UserDTO u ON r.userSeq = u.userSeq WHERE r.burgerSeq = :burgerSeq ORDER BY r.ratingSeq DESC")
+	List<RatingDTO> findAllByBurgerSeqOrderByRatingSeqDesc(@Param("burgerSeq") long burgerSeq);
 	
 }
