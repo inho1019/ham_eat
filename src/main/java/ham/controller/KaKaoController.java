1969106d5e08983a0c1a8c50509b642084b39a0a
simin <inho1019@naver.com>
2024-02-06 15:59:26 +0900

    카카오  검색 리스트 가져오기

--- src/main/java/ham/controller/KaKaoController.java
+++ src/main/java/ham/controller/KaKaoController.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ham.service.KakaoService;
@@ -15,8 +16,8 @@ public class KaKaoController {
 	@Autowired
 	private KakaoService kakaoService;
 	
-	@GetMapping("map/search/{search}")
-	public Object search(@PathVariable String search) throws URISyntaxException {
+	@GetMapping("map/search")
+	public Object search(@RequestParam("search") String search ) throws URISyntaxException {
 		return kakaoService.searchPlaces(search);
 	}
 }
