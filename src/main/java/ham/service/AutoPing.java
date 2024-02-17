392e0e44848dde69354c1d37c7c15cf401bf1483
simin <inho1019@naver.com>
2024-02-17 17:34:12 +0900

    업데이트

--- src/main/java/ham/service/AutoPing.java
+++ src/main/java/ham/service/AutoPing.java
@@ -1,13 +1,17 @@
 package ham.service;
-
+import java.net.URISyntaxException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class AutoPing {
 
-    @Scheduled(fixedRate = 300000) // 5분마다 실행
-    public void ping() {
-        System.out.println("Ping");
+	@Scheduled(fixedRate = 300000) // 5분마다 실행
+    public void autoPing() throws URISyntaxException {
+        String url = "https://hameat.onrender.com/ping";
+        ResponseEntity<Void> responseEntity = new RestTemplate().getForEntity(url, Void.class);
+        System.out.println(responseEntity);
     }
 }
